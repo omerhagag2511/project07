109aebe90368acfa001c70bead5050530dd2ed05
omerhagag2511 <omerhagag2511@gmail.com>
2026-01-16 08:23:13 +0200

    commit

--- app/src/main/java/com/example/project07/MainActivity.java
+++ app/src/main/java/com/example/project07/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project07;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iVMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    }
+    iVMain = (imageView) findViewById(R.id.iVMain);
+
+    public void go(View view) {
+
     }
 }
