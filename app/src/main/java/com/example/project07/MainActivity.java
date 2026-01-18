47bd76d48879d29b6df19fb02fbbc1da3562c0b7
omerhagag2511 <omerhagag2511@gmail.com>
2026-01-18 14:52:53 +0200

    2d commit

--- app/src/main/java/com/example/project07/MainActivity.java
+++ app/src/main/java/com/example/project07/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.project07;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -11,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView iVMain;
+    public ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-    iVMain = (imageView) findViewById(R.id.iVMain);
+    image = findViewById(R.id.image);
 
     public void go(View view) {
 
