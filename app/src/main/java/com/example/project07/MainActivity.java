c054d454fcdff44fa757674d8a2177518afb499c
omerhagag2511 <omerhagag2511@gmail.com>
2026-01-19 09:31:32 +0200

    3rd commit

--- app/src/main/java/com/example/project07/MainActivity.java
+++ app/src/main/java/com/example/project07/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.project07;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,24 +12,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     public ImageView image;
+    public Button text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         image = findViewById(R.id.image);
-
+        text = findViewById(R.id.button);
     }
     public void go(View view) {
         int num = (int) (Math.random() * 3);
         if (num == 2){
             image.setImageResource(R.drawable.image3);
+            text.setText("3");
         }
         else if (num == 1){
             image.setImageResource(R.drawable.image2);
+            text.setText("2");
         }
         else{
             image.setImageResource(R.drawable.image1);
+            text.setText("1");
         }
     }
 }
